b719561d0f0cb1cc0a3c0a22fc1fea488705f8e1
omarA4raf <omar.nasr303@gmail.com>
2023-11-11 01:50:01 +0200

    Initial commit

---
+++ modules/game_board.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pygame
+
+BLUE = (0, 76, 153)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+SQUARESIZE = 100
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT + 1) * SQUARESIZE
+size = (width, height)
+RADIUS = int(SQUARESIZE / 2 - 5)
+screen = pygame.display.set_mode(size)
+
+
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+def number_of_connected_fours(board, piece):
+    n = 0
+    # check horizontal locations for win
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+                n += 1
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
+                n += 1
+    # Check positively sloped diaganols
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
+                n += 1
+    # Check negatively sloped diaganols
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
+                n += 1
+    return n
+
+
+def check_winner(numbers_of_connected_fours_player1, numbers_of_connected_fours_player2):
+    print("numbers_of_connected_fours_player1 = ", numbers_of_connected_fours_player1)
+    print("numbers_of_connected_fours_player2 = ", numbers_of_connected_fours_player2)
+    if numbers_of_connected_fours_player1 > numbers_of_connected_fours_player2:
+        print("Player 1 wins!!! congrats")
+    elif numbers_of_connected_fours_player2 > numbers_of_connected_fours_player1:
+        print("Player 2 wins!!! congrats")
+    else:
+        print("No winner")
+
+
+def is_full_board(board):
+    for c in range(COLUMN_COUNT):
+        if board[ROW_COUNT - 1][c] == 0:
+            return False
+    return True
+
+
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, WHITE, (
+                int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (
+                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (
+                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+    pygame.display.update()
---
+++ modules/main.py
@@ -0,0 +1,98 @@
+import pygame
+import sys
+import math
+from game_board import create_board, drop_piece, is_valid_location, get_next_open_row, print_board, \
+    number_of_connected_fours, check_winner, is_full_board, draw_board
+
+COLUMN_COUNT = 7
+ROW_COUNT = 6
+BLUE = (0, 76, 153)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+def main():
+    board = create_board()
+    print_board(board)
+    full_board = is_full_board(board)
+    turn = 0
+
+    pygame.init()
+
+    SQUARESIZE = 100
+
+    width = COLUMN_COUNT * SQUARESIZE
+    height = (ROW_COUNT+1) * SQUARESIZE
+
+    size = (width, height)
+
+    RADIUS = int(SQUARESIZE/2 - 5)
+
+    screen = pygame.display.set_mode(size)
+    draw_board(board)
+    pygame.display.update()
+
+    myfont = pygame.font.SysFont("monospace", 75)
+
+    while not full_board:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, WHITE, (0, 0, width, SQUARESIZE))
+                posx = event.pos[0]
+                if turn == 0:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+                else:
+                    pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, WHITE, (0, 0, width, SQUARESIZE))
+                # print(event.pos)
+                # Ask for Player 1 Input
+                if turn == 0:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 1)
+                        full_board = is_full_board(board)
+
+                # # Ask for Player 2 Input
+                else:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 2)
+                        full_board = is_full_board(board)
+
+                print_board(board)
+                draw_board(board)
+
+                turn += 1
+                turn = turn % 2
+
+                if full_board:
+                    numbers_of_connected_fours_player1 = number_of_connected_fours(board, 1)
+                    numbers_of_connected_fours_player2 = number_of_connected_fours(board, 2)
+                    check_winner(numbers_of_connected_fours_player1, numbers_of_connected_fours_player2)
+                    if numbers_of_connected_fours_player1 > numbers_of_connected_fours_player2:
+                        label = myfont.render("Player 1 wins!!", 1, RED)
+                        screen.blit(label, (40, 10))
+                    elif numbers_of_connected_fours_player2 > numbers_of_connected_fours_player1:
+                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40, 10))
+                    else:
+                        label = myfont.render("No winner!!", 1, RED)
+                        screen.blit(label, (40, 10))
+                    pygame.display.update()
+                    pygame.time.wait(5000)
+
+if __name__ == "__main__":
+    main()
